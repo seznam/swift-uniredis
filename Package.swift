7e809221d42cf8f3294d9d2af4018140e1d06e59
Daniel Fojt <daniel.fojt2@firma.seznam.cz>
2020-05-22 09:00:19 +0200

    Bump dependencies in Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -8,11 +8,12 @@ let package = Package(
 		.library(name: "UniRedis", targets: ["UniRedis"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/seznam/swift-unisocket", from: "0.13.2"),
-		.package(url: "https://github.com/seznam/swift-resolver", from: "0.2.0")
+		.package(name: "UniSocket", url: "https://github.com/seznam/swift-unisocket", from: "0.14.0"),
+		.package(name: "Resolver", url: "https://github.com/seznam/swift-resolver", from: "0.3.0")
 	],
 	targets: [
 		.target(name: "UniRedis", dependencies: ["UniSocket", "Resolver"]),
 		.testTarget(name: "UniRedisTests", dependencies: ["UniRedis", "UniSocket"])
-	]
+	],
+	swiftLanguageVersions: [.v5]
 )
